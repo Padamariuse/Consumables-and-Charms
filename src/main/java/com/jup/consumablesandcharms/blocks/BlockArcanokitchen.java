5502212d5a8f4439a60e1c76d9c9475683128ee9
Padamariuse <noterrik@gmail.com>
2019-07-12 11:16:31 -0400

    arcanokitchen

--- src/main/java/com/jup/consumablesandcharms/blocks/BlockArcanokitchen.java
+++ src/main/java/com/jup/consumablesandcharms/blocks/BlockArcanokitchen.java
@@ -1,13 +1,22 @@
 package com.jup.consumablesandcharms.blocks;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
 import net.minecraftforge.common.ToolType;
 
 public class BlockArcanokitchen extends Block
 {
+    protected static final VoxelShape SHAPE = VoxelShapes.or(Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16D, 9/16D, 16D), Block.makeCuboidShape(7.0D, 0.0D, 7.0D, 9.0D, 14.0D, 9.0D));
     public BlockArcanokitchen()
     {
         super(Properties.create(Material.ROCK).hardnessAndResistance(3).sound(SoundType.STONE));
@@ -16,5 +25,14 @@ public class BlockArcanokitchen extends Block
     @Override
     public ToolType getHarvestTool(BlockState state) {
         return ToolType.PICKAXE;
+    }
+    
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPE;
+    }
+    
+    public BlockRenderLayer getRenderLayer() {
+        return BlockRenderLayer.CUTOUT;
     }
 }
--- src/main/java/com/jup/consumablesandcharms/items/ConsumablesAndCharmsItems.java
+++ src/main/java/com/jup/consumablesandcharms/items/ConsumablesAndCharmsItems.java
@@ -3,14 +3,14 @@ package com.jup.consumablesandcharms.items;
 import com.jup.consumablesandcharms.ConsumablesAndCharms;
 import com.jup.consumablesandcharms.items.foods.ItemCandycicle;
 import com.jup.consumablesandcharms.items.foods.ItemHolyCow;
-import net.minecraft.item.Food;
-import net.minecraft.item.Foods;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.block.Block;
+import net.minecraft.item.*;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.IForgeRegistry;
+
+import static com.jup.consumablesandcharms.blocks.ConsumablesAndCharmsBlocks.*;
 
 public class ConsumablesAndCharmsItems
 {
@@ -22,8 +22,15 @@ public class ConsumablesAndCharmsItems
     {
         IForgeRegistry<Item> registry = event.getRegistry();
         
+        registerBlockItem(registry, new BlockItem(ARCANOKITCHEN, new Item.Properties().group(ConsumablesAndCharmsItemGroup.MAIN)));
         registry.register(CANDYCICLE = new ItemCandycicle(new Item.Properties().group(ConsumablesAndCharmsItemGroup.MAIN).food(ConsumablesAndCharmsFoods.candycicle)).setRegistryName(location("candycicle")));
         registry.register(HOLY_COW = new ItemHolyCow(new Item.Properties().group(ConsumablesAndCharmsItemGroup.MAIN).food(ConsumablesAndCharmsFoods.holyCow)).setRegistryName(location("holy_cow")));
+    }
+    
+    private static Item registerBlockItem(IForgeRegistry<Item> registry, BlockItem item)
+    {
+        registry.register(item.setRegistryName(item.getBlock().getRegistryName()));
+        return item;
     }
     
     private static ResourceLocation location(String name)
